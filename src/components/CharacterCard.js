ce1877b18f42250657623f707d097dcc3c52a667
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-20 12:27:29 -0500

    completed the characters card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+    <div>
+      <h1>Name: {props.name} </h1>
+      <h2>Species: {props.species} </h2>
+      <h2> From: {props.from} </h2>
+    </div>
+  )
 }
