37a48f644ea15d39a77f64881017f7c4bc999217
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-20 12:38:52 -0500

    list displaying and working

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
  
+  const handleChanges = e => {
+    props.setQuery(e.target.value)
+  }
+
   return (
     <section className="search-form">
-     // Add a search form here
+     {/* // Add a search form here */}
+     <label htmlFor="search">Search: </label>
+     <input 
+      type="text"
+      placeholder="Search characters.."
+      name="search"
+      id="search"
+      onChange={handleChanges} />
     </section>
   );
 }
